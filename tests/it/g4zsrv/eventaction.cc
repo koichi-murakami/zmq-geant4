5844bc578b54d034e6f301892831372618190907
Koichi Murakami <koichi.murakami@kek.jp>
2017-09-07 03:05:06 +0900

    remove time history info

--- tests/it/g4zsrv/eventaction.cc
+++ tests/it/g4zsrv/eventaction.cc
@@ -8,21 +8,15 @@ This software is distributed WITHOUT ANY WARRANTY; without even the
 implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the License for more information.
 ============================================================================*/
-#include <sstream>
-#include <string>
 #include "G4Event.hh"
 #include "eventaction.h"
-#include "util/timehistory.h"
 
 // --------------------------------------------------------------------------
 namespace {
 
-TimeHistory* gtimer = nullptr;
-
-void ShowProgress(int nprocessed, const std::string& key) {
-  std::cout << "[MESSAGE] event-loop check point: "
-            << nprocessed << " events processed." << std::endl;
-  gtimer-> ShowHistory(key);
+void ShowProgress(int nprocessed) {
+  G4cout << "[MESSAGE] event-loop check point: "
+        << nprocessed << " events processed." << std::endl;
 }
 
 } // end of namespace
@@ -30,7 +24,6 @@ void ShowProgress(int nprocessed, const std::string& key) {
 // --------------------------------------------------------------------------
 EventAction::EventAction()
 {
-  ::gtimer = TimeHistory::GetTimeHistory();
 }
 
 // --------------------------------------------------------------------------
@@ -41,10 +34,6 @@ EventAction::~EventAction()
 // --------------------------------------------------------------------------
 void EventAction::BeginOfEventAction(const G4Event* event)
 {
-  int ievent = event-> GetEventID();
-  if ( ievent == 0 ) {
-    ::gtimer-> TakeSplit("FirstEventStart");
-  }
 }
 
 // --------------------------------------------------------------------------
@@ -55,9 +44,6 @@ void EventAction::EndOfEventAction(const G4Event* event)
 
   if ( ievent % kKiloEvents == 0 && ievent != 0 ) {
     int event_in_mega = ievent / kKiloEvents;
-    std::stringstream key;
-    key << "EventCheckPoint:" << event_in_mega << "K";
-    ::gtimer-> TakeSplit(key.str());
-    ::ShowProgress(ievent, key.str());
+    ::ShowProgress(ievent);
   }
 }
