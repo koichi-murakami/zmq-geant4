8ef3b9c1c5048988b79ec5509c450cc98c406681
Koichi Murakami <koichi.murakami@kek.jp>
2017-09-07 03:05:21 +0900

    remove json parts

--- tests/it/g4zsrv/appbuilder.cc
+++ tests/it/g4zsrv/appbuilder.cc
@@ -8,8 +8,6 @@ This software is distributed WITHOUT ANY WARRANTY; without even the
 implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the License for more information.
 ============================================================================*/
-#include <vector>
-#include <boost/lexical_cast.hpp>
 #include "FTFP_BERT.hh"
 #include "G4Navigator.hh"
 #include "G4ParticleGun.hh"
@@ -21,12 +19,10 @@ See the License for more information.
 #include "G4VPhysicalVolume.hh"
 #include "G4VSolid.hh"
 #include "CLHEP/Random/MTwistEngine.h"
-//#include "CLHEP/Random/Ranlux64Engine.h"
 #include "CLHEP/Random/Random.h"
 #include "appbuilder.h"
 #include "ecalgeom.h"
 #include "particlegun.h"
-#include "util/jsonparser.h"
 #include "eventaction.h"
 #include "runaction.h"
 #include "simdata.h"
@@ -37,7 +33,6 @@ namespace {
 
 G4RunManager* run_manager = nullptr;
 G4UImanager* ui_manager = nullptr;
-JsonParser* jparser = nullptr;
 
 // --------------------------------------------------------------------------
 void SetupGeomtry(SimData* data)
@@ -50,13 +45,7 @@ void SetupGeomtry(SimData* data)
 // --------------------------------------------------------------------------
 G4ThreeVector GetPrimaryPosition()
 {
-  G4ThreeVector pos = G4ThreeVector();
-  if ( jparser-> Contains("Primary/position") ) {
-    std::vector<double> dvec;
-    dvec.clear();
-    jparser-> GetDoubleArray("Primary/position", dvec);
-    pos = G4ThreeVector(dvec[0]*cm, dvec[1]*cm, dvec[2]*cm);
-  }
+  G4ThreeVector pos = G4ThreeVector(0., 0., -45.*cm);
   return pos;
 }
 
@@ -67,21 +56,16 @@ void SetupParticleGun()
   run_manager-> SetUserAction(pga);
   G4ParticleGun* gun = pga-> GetGun();
 
-  std::string pname = jparser-> GetStringValue("Primary/particle");
+  std::string pname = "e-";
   G4ParticleTable* ptable = G4ParticleTable::GetParticleTable();
   G4ParticleDefinition* pdef = ptable-> FindParticle(pname);
   if ( pdef != nullptr ) gun-> SetParticleDefinition(pdef);
 
-  double pkin = jparser-> GetDoubleValue("Primary/energy");
+  double pkin = 100.*MeV;
   gun-> SetParticleEnergy(pkin*MeV);
 
-  std::vector<double> dvec;
-  if ( jparser-> Contains("Primary/direction") ) {
-    dvec.clear();
-    jparser-> GetDoubleArray("Primary/direction", dvec);
-    G4ThreeVector pvec(dvec[0], dvec[1], dvec[2]);
-    gun-> SetParticleMomentumDirection(pvec);
-  }
+  G4ThreeVector pvec(0., 0., 1.);
+  gun-> SetParticleMomentumDirection(pvec);
 
   G4ThreeVector pos = GetPrimaryPosition();
   gun-> SetParticlePosition(pos);
@@ -101,18 +85,12 @@ AppBuilder::AppBuilder()
 {
   ::run_manager = G4RunManager::GetRunManager();
   ::ui_manager = G4UImanager::GetUIpointer();
-  ::jparser = JsonParser::GetJsonParser();
 
   simdata_ = new SimData;
 
   CLHEP::MTwistEngine* rand_engine = new CLHEP::MTwistEngine();
-  //CLHEP::Ranlux64Engine* rand_engine = new CLHEP::Ranlux64Engine();
 
-  long seed = 0L;
-  if ( jparser-> Contains("Run/Seed") ) {
-    long seed = jparser-> GetLongValue("Run/Seed");
-  }
-
+  const long seed = 987654321L;
   const int kK = 12345;
   rand_engine-> setSeed(seed, kK);
   CLHEP::HepRandom::setTheEngine(rand_engine);
